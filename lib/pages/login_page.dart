8bc843a11144aa96049b70ea58eb877349365583
hanan0998 <towertechnologies89@gmail.com>
2023-04-14 18:23:40 +0500

    Day 6 Complted!

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import '../utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
@@ -17,7 +24,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -34,6 +41,11 @@ class LoginPage extends StatelessWidget {
                         label: Text("Username"),
                         hintText: "Enter username",
                       ),
+                      // take the name from textfield and assign to name
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       // we don't want that password will show so we use this
@@ -46,15 +58,52 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // to jump on the homepage and we are using navgator.pushNamed
-                        // to push page by its name
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        // using await and sync to delay for 1 second and then move to the homepage
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homePage);
                       },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(120, 35)),
-                    )
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        height: 50,
+                        width: changeButton ? 50 : 120,
+
+                        // to align the text in center
+                        alignment: Alignment.center,
+                        // using this condition so that the login text will
+                        // into icon when button is pressed
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            // to add shape to container
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8)),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // to jump on the homepage and we are using navgator.pushNamed
+                    //     // to push page by its name
+                    //     Navigator.pushNamed(context, MyRoutes.homePage);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(120, 35)),
+                    // )
                   ],
                 ),
               ),
